1863351bd80698f72c0955c7dd6919d5701cd654
Brec Carson <brec@me.com>
2013-03-29 11:16:39 -0300

    Fixing install script.
    Also, instead of deleting the original setting preferences they are 
    only sent to the trashbin.

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
 git clone git@github.com:brec-c/dev-env.git Custom
-rm User/Preferences.sublime-settings
 
-ln -s ../Custom/Preferences.sublime-settings User/Preferences.sublime-settings
+cd User
+mv Preferences.sublime-settings ~/.Trash
+
+ln -s ../Custom/Preferences.sublime-settings
